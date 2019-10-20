5ac2eea75d3cd94c0bf2932613966dd64cf16a3d
unknown <grantludwig97@gmail.com>
2019-10-19 20:57:38 -0700

    initial commit

---
+++ ludwig_grant_AS09.py
@@ -0,0 +1,8 @@
+# File Name: ludwig_grant_AS05.py
+# File Path: /home/ludwigg/Python/PyRpi_AS9/ludwig_grant_AS09.py
+# Run Command: sudo python3 /home/ludwigg/Python/PyRpi_AS9/ludwig_grant_AS09.py
+
+# Grant Ludwig
+# 10/25/2019
+# AS.09
+# Have three balls bounce in a line
